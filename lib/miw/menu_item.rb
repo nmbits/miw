f8bc7dbb77b8f3edf9b6817b9b1e9091c5669141
Nobumitsu Takaoka <nmbits@gmail.com>
2020-01-14 12:13:27 +0900

    MiW::MenuItem: appearance added

--- lib/miw/menu_item.rb
+++ lib/miw/menu_item.rb
@@ -14,9 +14,10 @@ module MiW
       @frame = Rectangle.new 0, 0, 1, 1
       @highlight = false
       @enable = true
+      @appearance = :long
     end
     attr_reader :label, :frame, :shortcut, :accel_key, :icon, :type
-    attr_accessor :menu, :highlight
+    attr_accessor :menu, :highlight, :appearance
 
     def enable=(v = true)
       @enable = v ? true : false
@@ -43,13 +44,17 @@ module MiW
       cairo.rectangle frame.x, frame.y, frame.width, frame.height
       cairo.set_source_color bgcolor
       cairo.fill
-      panl = menu.pango_layout
-      panl.text = @label
-      x = @frame.x + @icon_width + 10 # pseudo
-      y = @frame.y + ((@frame.height - @frame.height / EXTENT_RATIO) / 2).to_i
-      cairo.move_to x, y
       cairo.set_source_color fgcolor
-      cairo.show_pango_layout panl
+      case @appearance
+      when :long
+        panl = menu.pango_layout
+        panl.text = @label
+        x = @frame.x + @icon_width + 10 # pseudo
+        y = @frame.y + ((@frame.height - @frame.height / EXTENT_RATIO) / 2).to_i
+        cairo.move_to x, y
+        cairo.show_pango_layout panl
+      when :short
+      end
     end
 
     def resize_to_preferred
@@ -57,9 +62,13 @@ module MiW
         panl = menu.pango_layout
         panl.text = @label
         w, h = panl.pixel_size
-        @icon_width = h
+        case @appearance
+        when :long
+          @icon_width = h
+          w += @icon_width
+        when :short
+        end
         w += 20 #pseudo
-        w += @icon_width
         h = (h * EXTENT_RATIO).ceil
         frame.resize_to w, h
       end
