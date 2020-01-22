3e1a368c760cd4b1d1aa812dc396e2bf8e64b6e6
Nobumitsu Takaoka <nmbits@gmail.com>
2020-01-22 19:10:03 +0900

    example/split_00.rb vertical test

--- example/split_00.rb
+++ example/split_00.rb
@@ -6,25 +6,29 @@ require 'miw/text_view'
 
 if __FILE__ == $0
 
+  orientation = ARGV[0] == 'vertical' ? :vertical : :horizontal
+
   w = MiW::Window.new("menu", 10, 10, 400, 400, layout: MiW::Layout::VBox)
   def w.quit_requested
     p :quit_requested
     EM.stop_event_loop
   end
 
-  v = MiW::SplitView.new "split"
+  v = MiW::SplitView.new "split", orientation: orientation
   v1 = MiW::TextView.new "text1"
-  v1.resize_to 50, 1
+  v1.resize_to 50, 50
   v1.set_text "1"
   v2 = MiW::TextView.new "text2"
-  v2.resize_to 50, 1
-  v1.set_text "2"
+  v2.resize_to 50, 50
+  v2.set_text "2"
   v3 = MiW::TextView.new "text3"
-  v3.resize_to 50, 1
-  v1.set_text "3"
-  v.add_child v1, resize: [false, true]
+  v3.resize_to 50, 50
+  v3.set_text "3"
+
+  resize_hint = orientation == :vertical ? [true, false] : [false, true]
+  v.add_child v1, resize: resize_hint
   v.add_child v2, resize: [true, true]
-  v.add_child v3, resize: [false, true]
+  v.add_child v3, resize: resize_hint
   w.add_child v, resize: [true, true]
 
   w.show
