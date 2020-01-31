02f41d1aa5d4fad4b5d5028e13cefe3d368ae2c0
Nobumitsu Takaoka <nmbits@gmail.com>
2020-01-31 19:34:09 +0900

    MiW::SplitView: new example

---
+++ example/split_01.rb
@@ -0,0 +1,37 @@
+require 'bundler/setup'
+require 'miw'
+require 'miw/window'
+require 'miw/split_view'
+require 'miw/text_view'
+
+if __FILE__ == $0
+
+  orientation = ARGV[0] == 'vertical' ? :vertical : :horizontal
+
+  w = MiW::Window.new("menu", 10, 10, 400, 400, layout: MiW::Layout::VBox)
+  def w.quit_requested
+    p :quit_requested
+    EM.stop_event_loop
+  end
+
+  v = MiW::SplitView.new "split", orientation: orientation
+
+  8.times do |i|
+    t = MiW::TextView.new "text_#{i}"
+    t.set_text i.to_s
+    resize = if i.even?
+               [true, true]
+             elsif orientation == :vertical
+               [true, false]
+             else
+               [false, true]
+             end
+    max_size = i % 4 == 2 ? [Float::INFINITY, Float::INFINITY] : [100, 100]
+    v.add_child t, resize: resize, min_size: [20, 20], max_size: max_size
+  end
+
+  w.add_child v, resize: [true, true]
+  w.show
+  MiW.run
+
+end
