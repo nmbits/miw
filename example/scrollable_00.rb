1ed7156a1518ae0a0f33462c1c98e7dcfc9a4838
Nobumitsu Takaoka <nmbits@gmail.com>
2021-11-27 20:05:01 +0900

    example/scrollable_00.rb use ScrollView

--- example/scrollable_00.rb
+++ example/scrollable_00.rb
@@ -1,39 +1,41 @@
 # coding: utf-8
 require 'bundler/setup'
 require 'miw'
-require 'miw/scrollable'
+require 'miw/scroll_view'
 
 if __FILE__ == $0
 
   class Check < MiW::View
-    include MiW::Scrollable
-    BOX_SIZE = 50
-    EXTENT_SIZE = 1000
-    RATIO_MIN = 0.1
-    RATIO_MAX = 10.0
+    BOX_SIZE = 20
+    EXTENT_SIZE = 500
     def initialize(id)
       super id
-      @view_port = MiW::Rectangle.new(0, 0, 100, 100)
-      @extent = MiW::Rectangle.new(0, 0, EXTENT_SIZE, EXTENT_SIZE)
-      @ratio = 1.0
-      initialize_scrollable true, true
     end
-    attr_reader :view_port, :extent
+
+    def calcurate_extent
+      MiW::Rectangle.new 0, 0, EXTENT_SIZE, EXTENT_SIZE
+    end
+
+    def adjust_view_point(x, y)
+      if x < 0 || extent.width < width
+        x = 0
+      elsif x > extent.right - width
+        x = extent.right - width
+      end
+      if y < 0 || extent.height < height
+        y = 0
+      elsif y > extent.bottom - height
+        y = extent.bottom - height
+      end
+      [x, y]
+    end
 
     def frame_resized(width, height)
-      rect = MiW::Rectangle.new 0, 0, width, height
-      nw = (rect.width * @ratio).to_i
-      nh = (rect.height * @ratio).to_i
-      @view_port.resize_to nw, nh
-      dx = dy = 0
-      if @view_port.right > EXTENT_SIZE
-        dx = EXTENT_SIZE - @view_port.right
+      x, y = adjust_view_point view_point.x, view_point.y
+      if x != view_point.x || y != view_point.y
+        scroll_to x, y
       end
-      if @view_port.bottom > EXTENT_SIZE
-        dy = EXTENT_SIZE - @view_port.bottom
-      end
-      @view_port.offset_by dx, dy
-      view_port_changed
+      trigger :bounds_changed
       invalidate
     end
 
@@ -48,69 +50,54 @@ if __FILE__ == $0
       end
     end
 
-    def mouse_up(*_)
-      ungrab_input
-      @dragging = false
+    def mouse_up(mx, my, button, status)
+      if button == 1
+        ungrab_input
+        @dragging = false
+      end
     end
 
     def mouse_moved(mx, my, transit, data)
       if @dragging
         dx = @px - mx
         dy = @py - my
-        x = [0, @view_port.x + dx].max
-        x = [x, @extent.width - @view_port.width].min
-        y = [0, @view_port.y + dy].max
-        y = [y, @extent.height - @view_port.height].min
-        @view_port.offset_to x, y
-        @px = mx
-        @py = my
-        view_port_changed
+        x = view_point.x + dx
+        y = view_point.y + dy
+        x, y = adjust_view_point(x, y)
+        scroll_to x, y
+        trigger :bounds_changed
         invalidate
       end
-      p MiW.get_mouse
     end
 
     def draw(rect)
-      dx = (@view_port.left % BOX_SIZE) * @ratio
-      dy = (@view_port.top % BOX_SIZE) * @ratio
-      # vertical
-      x = rect.x - dx
-      y = rect.y - dy
-      cairo.rectangle 0, 0, rect.width, rect.height
-      cairo.set_source_color MiW.colors[:control_background]
-      cairo.fill
-      while x < rect.width
-        if x >= 0
-          cairo.move_to x, 0
-          cairo.line_to x, rect.height
+      cairo.save do
+        cairo.rectangle rect.x, rect.y, rect.width, rect.height
+        cairo.clip
+        cairo.rectangle rect.x, rect.y, rect.width, rect.height
+        cairo.set_source_color MiW.colors[:control_background]
+        cairo.fill
+        limit_x = [bounds.right, EXTENT_SIZE].min
+        limit_y = [bounds.bottom, EXTENT_SIZE].min
+        y = view_point.y / BOX_SIZE * BOX_SIZE
+        while y < limit_y
+          x = view_point.x / BOX_SIZE * BOX_SIZE
+          while x < limit_x
+            if ((x / BOX_SIZE).odd?  && (y / BOX_SIZE).even?) ||
+               ((x / BOX_SIZE).even? && (y / BOX_SIZE).odd?)
+              cairo.rectangle x, y, BOX_SIZE, BOX_SIZE
+            end
+            x += BOX_SIZE
+          end
+          y += BOX_SIZE
         end
-        x += BOX_SIZE * @ratio
+        cairo.set_source_color MiW.colors[:control_background_highlight]
+        cairo.fill
       end
-      while y < rect.height
-        if y >= 0
-          cairo.move_to 0, y
-          cairo.line_to rect.width, y
-        end
-        y += BOX_SIZE * @ratio
-      end
-      cairo.set_source_color MiW.colors[:control_background_highlight]
-      cairo.stroke
-    end
-
-    def scroll_to(x, y)
-      @view_port.offset_to x, y
-      invalidate
-    end
-
-    def scroll_bars_rect
-      p __callee__
-      rect = bounds
-      rect.inset_by 20
-      rect
     end
   end
 
-  w = MiW::Window.new("test", 10, 10, 400, 400, layout: MiW::Layout::VBox)
+  w = MiW::Window.new "test", 10, 10, 400, 400, layout: MiW::Layout::VBox
   w.title = "scroll view"
   def w.quit_requested
     p :quit_requested
@@ -122,8 +109,12 @@ if __FILE__ == $0
   m.add_item MiW::MenuItem.new("Edit")
   w.add_child m, resize: [true, false]
 
+  sv = MiW::ScrollView.new :scroll, layout: MiW::Layout::VBox, vertical: true, horizontal: true
+
   v = Check.new :v
-  w.add_child v, resize: [true, true]
+  sv.set_target v, resize: [true, true]
+
+  w.add_child sv, resize: [true, true]
 
   w.show
   MiW.run
